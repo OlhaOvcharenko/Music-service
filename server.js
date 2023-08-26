ba64c12d899de09b9865ecfc27d6d144d2d928d9
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-08-26 13:38:19 +0300

    fix code for replit

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 /* global require, process */
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router('dist/db/app.json');
-const middlewares = defaults({
+import path from 'path';
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
+const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
-
 const port = process.env.PORT || 3131;
+
 server.use(middlewares);
 server.use(router);
+
 server.listen(port);
+
+export default server;
