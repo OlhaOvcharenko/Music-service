6d406088f0c78e68d986ecd2c3e8279e70d8bcaf
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-08-26 14:02:42 +0300

    preapare file for publication

--- server.mjs
+++ server.mjs
@@ -1,14 +1,17 @@
+import path from 'path';
+import jsonServer from 'json-server';
 
-/* global require, process */
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router('dist/db/app.json');
-const middlewares = defaults({
+const server = jsonServer.create();
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
+const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
-
 const port = process.env.PORT || 3131;
+
 server.use(middlewares);
 server.use(router);
+
 server.listen(port);
+
+export default server;
