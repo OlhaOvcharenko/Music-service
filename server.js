c0108b20805a8108dca96ff0a3cc58a012f8c75e
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-08-26 13:30:56 +0300

    update server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+/* global require, process */
+import { create, router as _router, defaults } from 'json-server';
+const server = create();
+const router = _router('dist/db/app.json');
+const middlewares = defaults({
+  static: 'dist',
+  noCors: true
+});
+
+const port = process.env.PORT || 3131;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
